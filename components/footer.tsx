59e23648e2abfd2a87c1ca64df1de7a4b9962698
amarnath666 <amarnathdhumal2001@gmail.com>
2025-12-21 15:07:49 +0530

    style: update footer text and shadow colors for better contrast

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,11 +6,11 @@ import ViewArea from "./ui/view-area";
 const Footer = () => {
     return (
         <ViewArea showBorderTop={false} className="!px-0 !py-2 sm:!py-4">
-            < footer className="w-full flex justify-center items-center bg-[#ffffff] dark:bg-[#000000] overflow-hidden transition-colors duration-300" >
+            <footer className="w-full flex justify-center items-center bg-[#ffffff] dark:bg-[#000000] overflow-hidden transition-colors duration-300" >
                 <div className="relative inline-block select-none isolate ">
                     {/* Main Text */}
                     <h1
-                        className="relative z-10 text-[#000] dark:text-[#C0C0C0] font-chillax font-semibold text-[55px] sm:text-[95px] md:text-[120px] leading-none tracking-wide transition-colors duration-300 "
+                        className="relative z-10 text-[#D3D3D3] dark:text-[#C0C0C0] font-chillax font-semibold text-[55px] sm:text-[95px] md:text-[120px] leading-none tracking-wide transition-colors duration-300 "
                         style={{
                             textShadow: "var(--footer-shadow, .03em .03em 0 #e5e5e5)"
                         }}
@@ -18,12 +18,12 @@ const Footer = () => {
                         AMARNATH
                         <style jsx>{`
                         :global(.dark) h1 {
-                            --footer-shadow: .03em .03em 0 #1a1a1a;
+                             --footer-shadow: .03em .03em 0 #1a1a1a;
                         }
                         
                         /* Fallback/Default for Light Mode */
                         h1 {
-                            --footer-shadow: .03em .03em 0 #000;
+                            --footer-shadow: .03em .03em 0 #C0C0C0;
                         }
                     `}</style>
                     </h1>
